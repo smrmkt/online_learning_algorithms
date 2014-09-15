62cb23671bb1c1190d8e6a628fe4a309ccf66381
smrmkt <smrmkt@gmail.com>
2014-09-15 20:27:56 +0900

    change to display graphs end of process

--- exec.py
+++ exec.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 
+from evaluator import Evaluator
 from loader import Loader
 import matplotlib.pyplot as plt
 from passive_aggressive import PassiveAggressive
 from passive_aggressive_1 import PassiveAggressive1
 from passive_aggressive_2 import PassiveAggressive2
+
+
+def graph_plot(plt_obj, show=False):
+    plt_obj.ylim(0, 1)
+    plt_obj.xlabel("Number of trials")
+    plt_obj.ylabel("Accuracy")
+    plt_obj.legend(["PA", "PA1", "PA2"], loc="lower right")
+    if show is True:
+        plt_obj.show()
+    else:
+        plt_obj.figure()
 
 
 if __name__ == '__main__':
@@ -18,35 +30,16 @@ if __name__ == '__main__':
     # training phase
     loader = Loader('a1a', 123, 30956, 1605)
     y_vec, feats_vec = loader.load_train()
-    count = [0, 0, 0]
-    accuracy = [[], [], []]
-    for i in range(len(y_vec)):
-        for j in range(3):
-            count[j] += pa[j].update(y_vec[i], feats_vec[i])
-            accuracy[j].append(100.0*count[j]/(i+1))
-    for i in range(3):
-        print count[i], len(y_vec), "{0:.2f}%".format(100.0*count[i]/len(y_vec))
-        plt.plot(accuracy[i])
-    plt.ylim(0, 100)
-    plt.xlabel("Number of trials")
-    plt.ylabel("Accuracy")
-    plt.legend(["PA", "PA1", "PA2"])
-    plt.show()
+    for i in range(len(pa)):
+        evaluator = Evaluator(pa[i], y_vec, feats_vec)
+        evaluator.update()
+        plt.plot(evaluator.accuracy)
+    graph_plot(plt)
 
     # test phase
     y_vec, feats_vec = loader.load_test()
-    count = [0, 0, 0]
-    accuracy = [[], [], []]
-    for i in range(len(y_vec)):
-        for j in range(3):
-            count[j] += 1 if y_vec[i]*pa[j].predict(feats_vec[i]) > 0 else 0
-            accuracy[j].append(100.0*count[j]/(i+1))
-    for i in range(3):
-        print count[i], len(y_vec), "{0:.2f}%".format(100.0*count[i]/len(y_vec))
-        plt.plot(accuracy[i])
-    plt.ylim(0, 100)
-    plt.xlabel("Number of trials")
-    plt.ylabel("Accuracy")
-    plt.legend(["PA", "PA1", "PA2"])
-    plt.show()
-
+    for i in range(len(pa)):
+        evaluator = Evaluator(pa[i], y_vec, feats_vec)
+        evaluator.predict()
+        plt.plot(evaluator.accuracy)
+    graph_plot(plt, show=True)
