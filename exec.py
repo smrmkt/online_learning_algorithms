5cba7d9f445230cf853802c2a18be9d2f79b871e
smrmkt <smrmkt@gmail.com>
2014-09-15 15:32:14 +0900

    add PA2

--- exec.py
+++ exec.py
@@ -4,6 +4,7 @@
 from loader import Loader
 from passive_aggressive import PassiveAggressive
 from passive_aggressive_1 import PassiveAggressive1
+from passive_aggressive_2 import PassiveAggressive2
 
 
 if __name__ == '__main__':
@@ -11,19 +12,22 @@ if __name__ == '__main__':
     loader = Loader('a1a', 123, 30956, 1605)
     y_vec, feats_vec = loader.load_train()
     pa = PassiveAggressive(feats_vec.shape[1])
-    pa.train(y_vec, feats_vec)
     pa1 = PassiveAggressive1(feats_vec.shape[1])
+    pa2 = PassiveAggressive2(feats_vec.shape[1])
+    pa.train(y_vec, feats_vec)
     pa1.train(y_vec, feats_vec)
+    pa2.train(y_vec, feats_vec)
 
     # test data
     y_vec, feats_vec = loader.load_test()
-    count_pa = 0
-    count_pa1 = 0
+    count = [0, 0, 0]
     for i in range(len(y_vec)):
-        count_pa += 1 if y_vec[i]*pa.predict(feats_vec[i]) > 0 else 0
-        count_pa1 += 1 if y_vec[i]*pa1.predict(feats_vec[i]) > 0 else 0
-    print count_pa, len(y_vec), "{0:.2f}%".format(100.0*count_pa/len(y_vec))
-    print count_pa1, len(y_vec), "{0:.2f}%".format(100.0*count_pa1/len(y_vec))
+        count[0] += 1 if y_vec[i]*pa.predict(feats_vec[i]) > 0 else 0
+        count[1] += 1 if y_vec[i]*pa1.predict(feats_vec[i]) > 0 else 0
+        count[2] += 1 if y_vec[i]*pa2.predict(feats_vec[i]) > 0 else 0
+    print count[0], len(y_vec), "{0:.2f}%".format(100.0*count[0]/len(y_vec))
+    print count[1], len(y_vec), "{0:.2f}%".format(100.0*count[1]/len(y_vec))
+    print count[2], len(y_vec), "{0:.2f}%".format(100.0*count[2]/len(y_vec))
 
 
 
---
+++ passive_aggressive_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import numpy as np
+
+
+class PassiveAggressive2:
+    def __init__(self, feat_dim, c=0.1):
+        self.t = 0
+        self.w = np.ones(feat_dim)
+        self.c = c
+
+    def train(self, y_vec, feats_vec):
+        for i in range(len(y_vec)):
+            self.update(y_vec[i], feats_vec[i,])
+
+    def predict(self, feats):
+        return np.dot(self.w, feats)
+
+    def update(self, y, feats):
+        l = max([0, 1-y*np.dot(self.w, feats)])
+        eta = l/(np.dot(feats, feats)+1/(2*self.c))
+        self.w += eta*y*feats
+        self.t += 1
+        return 1 if l == 0 else 0
+
+
+
