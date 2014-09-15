94727214e84c5a6c5b9077944122eb0cc9bafc76
smrmkt <smrmkt@gmail.com>
2014-09-15 15:12:38 +0900

    add PA1

--- exec.py
+++ exec.py
@@ -3,6 +3,7 @@
 
 from loader import Loader
 from passive_aggressive import PassiveAggressive
+from passive_aggressive_1 import PassiveAggressive1
 
 
 if __name__ == '__main__':
@@ -11,13 +12,18 @@ if __name__ == '__main__':
     y_vec, feats_vec = loader.load_train()
     pa = PassiveAggressive(feats_vec.shape[1])
     pa.train(y_vec, feats_vec)
+    pa1 = PassiveAggressive1(feats_vec.shape[1])
+    pa1.train(y_vec, feats_vec)
 
     # test data
     y_vec, feats_vec = loader.load_test()
-    count = 0
+    count_pa = 0
+    count_pa1 = 0
     for i in range(len(y_vec)):
-        count += 1 if y_vec[i]*pa.predict(feats_vec[i]) > 0 else 0
-    print count, len(y_vec), "{0:.2f}%".format(100.0*count/len(y_vec))
+        count_pa += 1 if y_vec[i]*pa.predict(feats_vec[i]) > 0 else 0
+        count_pa1 += 1 if y_vec[i]*pa1.predict(feats_vec[i]) > 0 else 0
+    print count_pa, len(y_vec), "{0:.2f}%".format(100.0*count_pa/len(y_vec))
+    print count_pa1, len(y_vec), "{0:.2f}%".format(100.0*count_pa1/len(y_vec))
 
 
 
